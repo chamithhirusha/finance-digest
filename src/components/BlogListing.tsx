2dddcb71741c38d15e8166b02ca1f582a5cf6c43
Chamith <152796091+chamithhirusha@users.noreply.github.com>
2026-02-09 11:50:11 +0530

    Added loading spinner svg

--- src/components/BlogListing.tsx
+++ src/components/BlogListing.tsx
@@ -3,7 +3,7 @@ import { TBlog } from "@/utils/types";
 import { useEffect, useRef, useState } from "react";
 import { BlogItem } from "./BlogItem";
 import { Alert } from "./Alert";
-import { SortIcon } from "./Icons";
+import { LoadingIcon, SortIcon } from "./Icons";
 
 const ITEMS_PER_LOAD = 8;
 
@@ -71,24 +71,26 @@ export const BlogListing = () => {
   return (
     <div className="flex flex-col">
       {/* Sort button */}
-      {visibleBlogs.length > 0 && <div className="flex justify-end gap-5">
-        <button
-          aria-label="Toggle sort order"
-          onClick={() =>
-            setSortOrder((prev) => (prev === "latest" ? "oldest" : "latest"))
-          }
-          className={`group flex justify-center items-center text-sm lg:text-md 
+      {visibleBlogs.length > 0 && (
+        <div className="flex justify-end gap-5">
+          <button
+            aria-label="Toggle sort order"
+            onClick={() =>
+              setSortOrder((prev) => (prev === "latest" ? "oldest" : "latest"))
+            }
+            className={`group flex justify-center items-center text-sm lg:text-md 
                   cursor-pointer gap-0 hover:gap-2 bg-black/10 md:bg-transparent hover:bg-white/10 outline-1 
                   outline-white/0 hover:outline-white/20 backdrop-blur-lg px-2 py-2 hover:px-3 hover:py-2 z-50 
                   rounded-full bg-black text-white transition-all duration-500 ease-out hover:scale-110 
                   hover:opacity-90`}
-        >
-          <SortIcon className="h-5 w-5 md:h-6 md:w-6 transform transition-transform duration-1000 ease-in-out scale-x-100 group-hover:scale-x-[-1]" />
-          <span className="overflow-hidden whitespace-nowrap max-w-0 opacity-0 transition-all duration-900 ease-in-out group-hover:max-w-[140px] group-hover:opacity-100">
-            {sortOrder === "latest" ? "Latest to older" : "Older to latest"}
-          </span>
-        </button>
-      </div>}
+          >
+            <SortIcon className="h-5 w-5 md:h-6 md:w-6 transform transition-transform duration-1000 ease-in-out scale-x-100 group-hover:scale-x-[-1]" />
+            <span className="overflow-hidden whitespace-nowrap max-w-0 opacity-0 transition-all duration-900 ease-in-out group-hover:max-w-[140px] group-hover:opacity-100">
+              {sortOrder === "latest" ? "Latest to older" : "Older to latest"}
+            </span>
+          </button>
+        </div>
+      )}
 
       <div className="flex flex-col">
         {/* Error alert */}
@@ -97,7 +99,12 @@ export const BlogListing = () => {
         )}
 
         {/* Loading state */}
-        {loading && <p className="mt-10 text-white/30">Loading blogs...</p>}
+        {loading && (
+          <div className="flex gap-5 mt-10">
+            <p className="text-white/50">Loading blogs</p>
+            <LoadingIcon />
+          </div>
+        )}
 
         {/* Blog grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-5 md:gap-y-20 gap-y-12 mt-10 md:mt-20">
@@ -116,9 +123,9 @@ export const BlogListing = () => {
         {page * ITEMS_PER_LOAD < blogData.length && (
           <div
             ref={loaderRef}
-            className="h-10 mt-10 flex justify-center items-center"
+            className="flex gap-5 h-10 mt-10 flex justify-center items-center"
           >
-            <p className="text-white/30">Loading more...</p>
+            <LoadingIcon />
           </div>
         )}
       </div>
